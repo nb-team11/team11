bec352ad9d3106f9e5621920e5b7bc9a3c5ed752
t2023-m0020 <t2023-m0020@t2023-m0020ui-MacBookAir.local>
2024-06-18 00:54:08 +0900

    fix: 메인페이지

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import MainNavBar from '../components/MainNavBar';
+
+function MainPage() {
+  return (
+    <>
+      <MainNavBar />
+    </>
+  );
+}
+
+export default MainPage;
