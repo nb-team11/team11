ab15357561321dc466aeca834c03f112d73f0b99
oneieo <sun020716@gmail.com>
2024-06-17 23:18:39 +0900

    update: add tags about comment

--- src/components/PostComment/PostComment.jsx
+++ src/components/PostComment/PostComment.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import {
+  StyleButton,
+  StyleCommentBox,
+  StyleCommentContainer,
+  StyleCommentContent,
+  StyleCommentCreatedat,
+  StyleCommentInputBox,
+  StyleCommentLeft,
+  StyleCommentList,
+  StyleCommentRegisterBox,
+  StyleCommentRight,
+  StyleCommentWriter,
+  StyleInput,
+  StyleLabel
+} from './PostComment.style';
 
 const PostComment = () => {
-  return <div>gg</div>;
+  return (
+    <>
+      <StyleCommentContainer>
+        <StyleCommentRegisterBox>
+          <StyleCommentInputBox>
+            <StyleLabel>닉네임</StyleLabel>
+            <StyleInput />
+          </StyleCommentInputBox>
+          <StyleCommentInputBox>
+            <StyleLabel>비밀번호</StyleLabel>
+            <StyleInput />
+          </StyleCommentInputBox>
+          <StyleCommentInputBox>
+            <StyleLabel>댓글</StyleLabel>
+            <StyleInput />
+          </StyleCommentInputBox>
+          <StyleButton>보내기</StyleButton>
+        </StyleCommentRegisterBox>
+        <StyleCommentList>
+          <StyleCommentBox>
+            <StyleCommentLeft>
+              <StyleCommentWriter>닉네임</StyleCommentWriter>
+              <StyleCommentContent>댓글내용</StyleCommentContent>
+              <StyleCommentCreatedat>작성시간</StyleCommentCreatedat>
+            </StyleCommentLeft>
+            <StyleCommentRight>
+              <StyleButton>수정</StyleButton>
+              <StyleButton>삭제</StyleButton>
+            </StyleCommentRight>
+          </StyleCommentBox>
+        </StyleCommentList>
+      </StyleCommentContainer>
+    </>
+  );
 };
 
 export default PostComment;
---
+++ src/components/PostComment/PostComment.style.jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const StyleCommentContainer = styled.div`
+  width: 1400px;
+  height: 500px;
+  margin: 16px auto;
+`;
+export const StyleCommentRegisterBox = styled.div`
+  width: 1400px;
+  height: 120px;
+  background-color: #7a7a7a;
+`;
+export const StyleCommentInputBox = styled.div`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleLabel = styled.label`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleInput = styled.input`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleButton = styled.button`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentList = styled.ul`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentBox = styled.li`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentLeft = styled.div`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentRight = styled.div`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentWriter = styled.p`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentContent = styled.p`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
+export const StyleCommentCreatedat = styled.p`
+  /* width: 1400px;
+  height: 500px;
+  margin: 0 auto; */
+`;
