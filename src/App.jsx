1384954a2eab88ceaaf1ee0fd0df8e482230b6e6
Minsu <alstn549@naver.com>
2024-06-18 20:43:14 +0900

    navbar fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
-import Navbar from './components/Navbar/Navbar';
-import reset from 'styled-reset';
-import { createGlobalStyle } from 'styled-components';
-import MapApI from './components/MapApi/MapApI';
 import AppRouter from './Router/Router';
 
-const Globalstyle = createGlobalStyle`
-  ${reset}
-`;
-
 const App = () => {
-  return (
-    <>
-      <Globalstyle />
-      <Provider store={store}>
-        <AppRouter />
-      </Provider>
-    </>
-  );
+  return <AppRouter />;
 };
 
 export default App;
